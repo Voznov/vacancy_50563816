24971a3941847f195eda13ceb2e04af35ca35229
Voznov <voznov2peter@gmail.com>
2022-04-06 00:59:38 +0300

    Added titles to screens

--- src/screens/index.js
+++ src/screens/index.js
@@ -4,8 +4,14 @@ import Comment from './Comment';
 export const SCREEN_DATA = {
   Posts: {
     component: Posts,
+    options: {
+      title: 'Посты',
+    },
   },
   Comment: {
     component: Comment,
+    options: {
+      title: 'Комментарии',
+    },
   },
 };
